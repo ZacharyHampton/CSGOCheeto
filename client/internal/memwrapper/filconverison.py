e55b8b0e04e521132e88474648416413d1164039
AireBear11 <dantesante2903@gmail.com>
2023-03-20 20:16:10 -0700

    dfgdfg

---
+++ client/internal/memwrapper/filconverison.py
@@ -0,0 +1,4 @@
+class TypeConversion:
+    def __init__(self, conversion_bytes: bytes):
+        pass
+
--- client/internal/memwrapper/memwrapper.py
+++ client/internal/memwrapper/memwrapper.py
@@ -1,13 +1,35 @@
 import pymem
 import memprocfs
+import time
+import struct
 
 
 class Memory:
     def __init__(self):
-        try:
-            self.vmm = memprocfs.Vmm(["-printf", "-v", "-device", "FPGA", "-memmap", "auto"])
-        except TypeError as e:
-            if "vmm failed" in str(e):
-                self.dma = False
 
-        self.dma = True
+        try:
+            self.vmm = memprocfs.Vmm(["-printf", "-device", "FPGA", "-memmap", "auto"])
+            self.dma = True
+        except TypeError as e:
+            self.dma = False
+
+        self.process = self.get_process()
+
+    def get_process(self):
+        while True:
+            try:
+                if self.dma:
+                    return self.vmm.process('csgo.exe')
+                else:
+                    return pymem.Pymem('csgo.exe')
+            except (RuntimeError, pymem.exception.ProcessNotFound):
+                time.sleep(1)
+
+    def read(self, address: int, size: int):
+        if self.dma:
+            return self.process.memory.read(address, size, memprocfs.FLAG_NOCACHE)
+        else:
+            return self.process.read_bytes(address, size)
+
+    def read_ptr(self, address: int):
+        return int.from_bytes(self.read(address, struct.calcsize("LL")), 'little')
