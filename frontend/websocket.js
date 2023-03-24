758cae9a85980a247326bda3ec227f573b129d32
AireBear11 <dantesante2903@gmail.com>
2023-03-23 17:32:50 -0700

    engine and player classes

--- frontend/websocket.js
+++ frontend/websocket.js
@@ -1,7 +1,8 @@
-const socket = new WebSocket("127.0.0.1:8000/ws/asdf");
-
-socket.onmessage = (info) => {
-    var json_object = JSON.parse(info.data);
-
+function onLoad() {
+    const socket = new WebSocket("127.0.0.1:8000/ws/asdf");
+    socket.onmessage = (info) => {
+        let json_object = JSON.parse(info.data);
+    }
 }
+
 
