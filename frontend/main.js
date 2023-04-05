7b24971222c9e160c84ba4f603ae345616a618aa
AireBear11 <dantesante2903@gmail.com>
2023-04-05 13:16:20 -0700

    radar :DDDDDD

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ function Drawing(players) {
     this.players = players;
 
 
-    this.create = (entity, team, x, y) => {
+    this.create = (entity, team) => {
         let players = document.getElementById('players')
         let player = document.createElement('div')
         player.id = entity
@@ -16,15 +16,14 @@ function Drawing(players) {
             default:
                 return null;
         }
-        player.style.left = -x + "px"
-        player.style.top = -y + "px"
         players.appendChild(player)
     };
 
 
     this.update = (playerElement, x, y, team) => {
-        playerElement.style.left = x + 'px';
-        playerElement.style.top = y + 'px';
+        let position = this.scale(x, y)
+        playerElement.style.left = position.x + 'px';
+        playerElement.style.top = position.y + 'px';
         switch (team) {
             case "3":
                 playerElement.className = "counter"
@@ -44,6 +43,19 @@ function Drawing(players) {
             element.remove()
         }
     }
+
+
+    this.scale = (x, y) => {
+        let maps = {'de_dust2.png':[-2476,3239,4.4] ,'de_inferno.png': [-2087,3870,4.9], 'de_vertigo.png':[-3168,1762,4], 'de_nuke.png': [-3453, 2887, 7], 'de_mirage.png':[-3230, 1713, 5]}
+        let imgheight = document.getElementById("map-img").height
+        let map = document.getElementById("map-img").src.match(/([^\/]*)\/*$/)[1]
+        let offsetx = maps[map][0]
+        let offsety = maps[map][1]
+        let gameScale = maps[map][2]
+        let xpos = (((parseInt(x)) - offsetx) * imgheight) / (gameScale*1024).toString();
+        let ypos = -(((parseInt(y)) - offsety) * imgheight) / (gameScale*1024).toString();
+        return {"x": xpos, "y": ypos}
+    }
 }
 
 
@@ -60,22 +72,23 @@ socket.onmessage = (event) => {
         drawing.clear("counter")
     }
 
+    if (data.status === "game_started") {
+         let a = data.map.name
+         document.getElementById('map-img').src = a + ".png"
+    }
 
     for (let player of data.players) {
 
         if (player.health > 0) {
             let playerElement = document.getElementById(player.steam_id)
-
-            let x = (parseInt(player.position.x) + 2087) / 6.3;
-            let y = -(parseInt(player.position.y) - 3870) / 6.3;
-            x = x.toString()
-            y = y.toString()
-
+            let y = player.position.y
             if (playerElement === null) {
-                drawing.create(player.steam_id, player.team, x, y)
+                drawing.create(player.steam_id, player.team)
+            } else {
+                drawing.update(playerElement, player.position.x, y, player.team)
             }
         } else {
-            playerElement.remove()
+            document.getElementById(player.steam_id).remove()
         }
     }
 }
